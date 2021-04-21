6f29e5c3f31fd63a387cd7d93ac52c116e99ffd7
jopymesgyre <bolinhao116027@126.com>
2021-04-21 17:29:04 +0800

    添加final

--- final_override.cpp
+++ final_override.cpp
@@ -46,6 +46,20 @@ private:
     double x, y, z;
 };
 
+struct Object {
+    virtual void fun() = 0;
+};
+
+//-----------------------------------------
+struct Base : public Object {
+    void fun() final;               //声明final不可写
+};
+
+struct Derived : public Base {
+    void fun();                     //无法通过编译
+}
+
+
 int main() {
     return 0;
 }
