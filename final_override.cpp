75a31be5031323eec62b03b9b7eb32d6b0200ce7
jopymesgyre <bolinhao116027@126.com>
2021-04-21 17:24:13 +0800

    函数重载

--- final_override.cpp
+++ final_override.cpp
@@ -12,4 +12,40 @@
 
 #include <stdlib.h>
 #include <iostream>
+using namespace std;
 
+class MathObject {
+public:
+    virtual double Arith() = 0;
+    virtual void Pirnt() = 0;
+};
+
+class Printable : public MathObject {
+public:
+    double Arith() = 0;
+    void Print() {
+        cout << "OutPut is: " << Arith() << endl;
+    }
+};
+  
+class Add2 : public Printable {
+public:
+    Add2(double a, double b) : x(a), y(b) { }
+    double Arith() {
+        return x + y;
+    }   
+private:
+    double x, y;
+};
+
+class Mu13 : public Printable {
+public:
+    Mu13(double a, double b, double c) : x(a), y(b), z(c) { }
+    double Arith() { return x * y * z; }
+private:
+    double x, y, z;
+};
+
+int main() {
+    return 0;
+}
