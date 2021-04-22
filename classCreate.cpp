72142155f45d0f24e33c7259fd99088a151ca89c
jopymesgyre <bolinhao116027@126.com>
2021-04-22 10:02:23 +0800

    传统的class构造函数派生

---
+++ classCreate.cpp
@@ -0,0 +1,61 @@
+/***********************************************************
+* 
+*     File Name:  classCreate.cpp
+* 
+*        Author:  Algolc
+* 
+*          mail:  17360224@qq.com
+* 
+*  Created Time:  Wed 18 Jul 2018 09:47:40 AM CST
+* 
+***********************************************************/
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+#if 0
+struct A {
+    A (int i) { }
+};
+
+struct B : public A {
+    B (int i) : A(i), d(i) { }
+    int d;
+};
+#endif
+
+struct A {
+    A (int i) { 
+
+    }
+
+    A (double d, int i) {
+
+    }
+    
+    A (float f, int i, const char* c) {
+
+    }
+};
+
+struct B {
+    B (int i) : A (i) {
+
+    }
+
+    B (double d, int i) : A (d, i){
+
+    }
+
+    B (float f, int i, const char* c) : A (f, i, c) {
+
+    }
+
+    //...
+
+    virtual void ExtraInterface() {
+
+    }
+};
+
