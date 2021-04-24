a5eebddc033a9a6df291dcb29eb4ed3da5383ea1
jopymesgyre <bolinhao116027@126.com>
2021-04-24 16:29:26 +0800

    auto的初次尝试

---
+++ autoHelloWorld.cpp
@@ -0,0 +1,22 @@
+/***********************************************************
+* 
+*     File Name:  autoHelloWorld.cpp
+* 
+*        Author:  Algolc
+* 
+*          mail:  17360224@qq.com
+* 
+*  Created Time:  Fri 20 Jul 2018 03:32:14 PM CST
+* 
+***********************************************************/
+
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    auto name = "world.\n";
+    cout << "hello, " << name;
+
+    return 0;
+}
